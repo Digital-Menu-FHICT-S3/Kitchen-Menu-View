596b6e5d19127e395a311dc8f0992ab526d065bc
DavidChang <57895305+Davidchang24@users.noreply.github.com>
2021-04-19 15:19:04 +0200

    removed tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,14 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// test('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 
 // test if element exists
 // test('renders Hello World', () => {
@@ -18,24 +17,24 @@ test('renders without crashing', () => {
 // });
 
 // boolean test
-describe('true is truthy and false is falsy', () => {
-  test('true is truthy', () => {
+describe("true is truthy and false is falsy", () => {
+  test("true is truthy", () => {
     expect(true).toBe(true);
   });
 
-  test('false is falsy', () => {
+  test("false is falsy", () => {
     expect(false).toBe(false);
   });
 });
 
-test('3 is 3', () => {
+test("3 is 3", () => {
   expect(3).toBe(3);
 });
 
-test('text is text', () => {
-  expect('text').toBe('text');
+test("text is text", () => {
+  expect("text").toBe("text");
 });
 
-test('text not is tekst', () => {
-  expect('text').not.toBe('tekst')
+test("text not is tekst", () => {
+  expect("text").not.toBe("tekst");
 });
