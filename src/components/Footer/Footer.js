f243ceae87f852722c24f45ed542a0b60837c1ef
DavidChang <57895305+Davidchang24@users.noreply.github.com>
2021-05-10 10:07:24 +0200

    added done and in-progress link for orders

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import "./Footer.sass"
+import React from "react";
+import "./Footer.sass";
 import Container from "react-bootstrap/Container";
 
 const Footer = () => {
-    return (
-        <div className="Footer">
-            <Container>
-                <div className="footer-container">
-                    <span>
-                        Footer
-                    </span>
-                </div>
-            </Container>
+  return (
+    <div className="footer">
+      <Container>
+        <div className="footer-container">
+          <span>Footer</span>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
