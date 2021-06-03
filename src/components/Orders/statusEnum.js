8d25044350526babcd3d2d52dc593d982729b9fa
Twan <piet2001@fireman.net>
2021-06-03 11:35:50 +0200

    fix orderstatus bug
    change enum for bugfix orderstatus filter

--- src/components/Orders/statusEnum.js
+++ src/components/Orders/statusEnum.js
@@ -1,5 +1,5 @@
-export var statusEnum = {
-    ToDo: 1,
-    InProgress: 2,
-    Done: 3
+export const statusEnum = {
+    ToDo: { value: 0, name: "ToDo" },
+    InProgress: { value: 1, name: "InProgress" },
+    Done: { value: 2, name: "Done" }
 }
