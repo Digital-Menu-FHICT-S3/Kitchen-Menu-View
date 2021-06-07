f02d8ac8256a9b364d00bf0e7b1a330c12bc21ed
Milan <58393068+Desoxyr@users.noreply.github.com>
2021-06-07 14:35:06 +0200

    Remove unnecessary NavLinks from Header which cause the user to be linked to localhost:3000/#. Fixed sass so the styling is component specific.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,44 +1,37 @@
 import "./Header.sass";
-import { Nav, Navbar } from "react-bootstrap";
-import { MdDone } from "react-icons/md";
-import { CgAdd } from "react-icons/cg";
-import { GrInProgress } from "react-icons/gr";
-import { GiKnifeFork, GiAppleSeeds } from "react-icons/gi";
-import { Link } from "react-router-dom";
+import {Nav, Navbar} from "react-bootstrap";
+import {MdDone} from "react-icons/md";
+import {GrInProgress} from "react-icons/gr";
+import {GiAppleSeeds} from "react-icons/gi";
+import {Link} from "react-router-dom";
 import React from "react";
 
-const Header = ({ headerTitle }) => {
-  return (
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Link to="/">
-          <Navbar.Brand> {headerTitle} </Navbar.Brand>
-        </Link>
+const Header = ({headerTitle}) => {
+    return (
+        <div className="header-wrapper">
+            <Navbar bg="light" expand="lg">
+                <Link to="/">
+                    <Navbar.Brand> {headerTitle} </Navbar.Brand>
+                </Link>
 
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link>
-              <Link className="link" to="/finished-orders">
-                <MdDone size={30} />
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link className="link" to="/current-orders">
-                <GrInProgress size={30} />
-              </Link>
-            </Nav.Link>
-          </Nav>
-          <Nav className="ml-auto">
-            <Nav.Link>
-              <Link className="link" to="/stock">
-                <GiAppleSeeds size={30} />
-              </Link>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Link className="link" to="/finished-orders">
+                            <MdDone size={30}/>
+                        </Link>
+                        <Link className="link" to="/current-orders">
+                            <GrInProgress size={30}/>
+                        </Link>
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Link className="link" to="/stock">
+                            <GiAppleSeeds size={30}/>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
 };
 
 export default Header;
